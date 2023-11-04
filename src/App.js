61240b68af5371ab3816a4b7469cb2398c2e5462
Adarsha-React <adarshareact2023@gmail.com>
2023-11-04 18:19:55 +0530

    dev_featureOne - #0001

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  console.log(process.env.REACT_APP_DEV_URL);
   return (
     <div>
-      <h1>Testing Environment files</h1>
-      <h1>GitHub action - workflow created</h1>
+      <h1>GitHub - changes from dev_featureOne to development branch</h1>
     </div>
   );
 }
